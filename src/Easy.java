a3ef2f7fd50ff0ca606db4b5d2f1bbf1c0dba76a
Татьяна <fokT.M@yandex.ru>
2022-09-16 22:54:13 +0300

    переделано

--- src/Easy.java
+++ src/Easy.java
@@ -1,68 +1,68 @@
 import java.util.Arrays;
 
-    public class Easy {
-        private final Employee[] employees;
+public class Easy {
+    private final Employee[] employees;
 
-        public Easy(Employee[] employees) {
-            this.employees = employees;
+    public Easy(Employee[] employees) {
+        this.employees = employees;
+    }
+
+    public void printEmployees() {
+        Arrays.stream(employees)
+                .forEach(System.out::println);
+    }
+
+    public int calculateTotalSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    public Employee findEmployeeWithMinSalary() {
+        Employee employeeWithMinSalary;
+
+        employeeWithMinSalary = employees[0];
+        for (Employee employee : employees) {
+            if (employee.getSalary() < employeeWithMinSalary.getSalary()) {
+                employeeWithMinSalary = employee;
+            }
         }
 
-        public void printEmployees() {
-            Arrays.stream(employees)
-                    .forEach(System.out::println);
+        return employeeWithMinSalary;
+    }
+
+    public Employee findEmployeeWithMaxSalary() {
+        Employee employeeWithMaxSalary;
+
+        employeeWithMaxSalary = employees[0];
+        for (Employee employee : employees) {
+            if (employee.getSalary() > employeeWithMaxSalary.getSalary()) {
+                employeeWithMaxSalary = employee;
+            }
         }
 
-        public int calculateTotalSalary() {
-            int sum = 0;
-            for (Employee employee : employees) {
+        return employeeWithMaxSalary;
+    }
+
+    public float calculateAverageSalary() {
+        int sum = 0;
+
+        for (Employee employee : employees) {
+            if (employee != null) {
                 sum += employee.getSalary();
             }
-            return sum;
         }
 
-        public Employee findEmployeeWithMinSalary() {
-            Employee employeeWithMinSalary;
+        return (float) sum / employees.length;
+    }
 
-            employeeWithMinSalary = employees[0];
-            for (Employee employee : employees) {
-                if (employee.getSalary() < employeeWithMinSalary.getSalary()) {
-                    employeeWithMinSalary = employee;
-                }
-            }
-
-            return employeeWithMinSalary;
-        }
-
-        public Employee findEmployeeWithMaxSalary() {
-            Employee employeeWithMaxSalary;
-
-            employeeWithMaxSalary = employees[0];
-            for (Employee employee : employees) {
-                if (employee.getSalary() > employeeWithMaxSalary.getSalary()) {
-                    employeeWithMaxSalary = employee;
-                }
-            }
-
-            return employeeWithMaxSalary;
-        }
-
-        public float calculateAverageSalary() {
-            int sum = 0;
-
-            for (Employee employee : employees) {
-                if (employee != null) {
-                    sum += employee.getSalary();
-                }
-            }
-
-            return (float) sum / employees.length;
-        }
-
-        public void printFullNames() {
-            for (Employee employee : employees) {
-                System.out.println(
-                        employee.getFirstName() + " " + employee.getLastName() + " " + employee.getMiddleName()
-                );
-            }
+    public void printFullNames() {
+        for (Employee employee : employees) {
+            System.out.println(
+                    employee.getFirstName() + " " + employee.getLastName() + " " + employee.getMiddleName()
+            );
         }
     }
+}
--- src/Employee.java
+++ src/Employee.java
@@ -74,3 +74,4 @@ public class Employee {
                 "Зарплата: '" + salary;
     }
 }
+
